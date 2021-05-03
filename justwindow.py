241b13b267111dae78cd8359b07c6ba3ea186d62
Song <master047@msn.com>
2021-05-03 16:29:39 +0900

    Update justwindow.py
    aws5

--- justwindow.py
+++ justwindow.py
@@ -4,6 +4,19 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
+
+def list_sns(region, creds, sns_topics=[]):
+    sns = boto_session('sns', creds, region)
+    response = sns.list_topics()
+    for topic_arn in response["Topics"]:
+        sns_topics.append(topic_arn["TopicArn"])
+    return sns_topics
+  
+def process():
+    for region, creds in jobs["auth_config"]:
+        arns = list_sns(region, creds)
+    return
+
 pygame.init()
 SURFACE = pygame.display.set_mode((400,300))
 pygame.display.set_caption("Just Window")
@@ -22,3 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
